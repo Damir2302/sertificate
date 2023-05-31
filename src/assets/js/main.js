e59dd4692de5fad8a0fd243df944bab1c60178b2
Damir2302 <damir23.02@mail.ru>
2023-05-31 17:25:04 +0300

    fixed

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
    // INPUT MASK PHONE NUMBER
-   $('input[type="tel"]').inputmask({"mask": "+7 (999) 999-99-99"})
+   $('input[type="tel"]').inputmask({"mask": "+7 (999) 999-99-99", "showMaskOnHover": false})
 
    // HEADER FIXED
    $(window).on('scroll', function() {
@@ -22,6 +22,61 @@ $(document).ready(function() {
             $('.header-fixed').addClass('header-backscroll')
         }
         lastScrollTop = currentScroll
+    })
+
+
+    // CLIENTS IMG SLIDER MOBILE
+    let clientSlider
+    let clientSliderInit
+    
+    function clientSliderRun() {
+        if ($(window).width() < 744) {
+            if(!clientSliderInit) {
+              clientSliderInit = true
+              clientSlider = new Swiper(`.services-clients .swiper`, {
+                    slidesPerView: 'auto',
+                    spaceBetween: 18,
+                    freeMode: true,
+                    slidesOffsetAfter: 0
+                })
+            }
+        } else {
+            if (typeof clientSlider !== "undefined") {
+              clientSliderInit = false
+              clientSlider.destroy()
+            }
+        }
+    }
+    
+    clientSliderRun()
+
+    $(window).on('resize', function() {
+      clientSliderRun()
+    })
+
+    // REVIEWS SLIDER
+    
+    let reviewsSlider = new Swiper(`.reviews .swiper`, {
+      slidesPerView: 1,
+      spaceBetween: 20,
+      loop: true,
+
+      navigation: {
+        nextEl: '.reviews .nav-arrow--right',
+        prevEl: '.reviews .nav-arrow--left',
+      },
+
+      breakpoints: {
+        744: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+
+        1260: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+        }
+      }
     })
 
 })
